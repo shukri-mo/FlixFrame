d409370f192049b678d81119c01acdd711d502c5
shukri mo <shukriiman83@gmail.com>
2025-09-08 14:50:37 +0800

    complete

---
+++ src/types/tvshow.ts
@@ -0,0 +1,65 @@
+export interface TVShow {
+  id: number;
+  name: string;
+  type: string;
+  language: string;
+  genres: string[];
+  status: string;
+  runtime: number | null;
+  averageRuntime: number | null;
+  premiered: string | null;
+  ended: string | null;
+  officialSite: string | null;
+  schedule: {
+    time: string;
+    days: string[];
+  };
+  rating: {
+    average: number | null;
+  };
+  weight: number;
+  network: {
+    id: number;
+    name: string;
+    country: {
+      name: string;
+      code: string;
+      timezone: string;
+    };
+    officialSite: string | null;
+  } | null;
+  webChannel: {
+    id: number;
+    name: string;
+    country: {
+      name: string;
+      code: string;
+      timezone: string;
+    } | null;
+    officialSite: string | null;
+  } | null;
+  dvdCountry: any;
+  externals: {
+    tvrage: number | null;
+    thetvdb: number | null;
+    imdb: string | null;
+  };
+  image: {
+    medium: string;
+    original: string;
+  } | null;
+  summary: string | null;
+  updated: number;
+  _links: {
+    self: {
+      href: string;
+    };
+  };
+}
+
+export interface SearchResult {
+  score: number;
+  show: TVShow;
+}
+
+export interface SearchResponse extends Array<SearchResult> {}
